870a6ec5ef10c2ba6218e1609e4483cc79eb0862
avirupdutta <finalguider99@gmail.com>
2020-09-22 21:48:27 +0530

    added bunch of favicons and refactored old code

--- static/js/main.js
+++ static/js/main.js
@@ -1,116 +1,85 @@
 function setup() {
-	console.log(CANVAS_WIDTH, CANVAS_HEIGHT);
-	canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-	canvas.parent("#canvasArea");
+    canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+    canvas.parent("#canvasArea");
 
-	socket = io.connect("http://localhost:3000");
-	socket.on("mouse", newDrawing);
+    socket = io.connect(window.location.href);
+    socket.on("mouse", newDrawing);
 
-	socket.on("clear", () => {
-		background(BACKGROUND_COLOR);
-	});
+    socket.on("clear", () => {
+        background(BACKGROUND_COLOR);
+    });
 
-	$("#clearScreen").click(clearScreen);
-	$("#brush").click(activateBrush);
-	$("#eraser").click(activateEraser);
-	$("#downloadImage").click(saveMyCanvas);
+    $("#clearScreen").click(clearScreen);
+    $("#brush").click(activateBrush);
+    $("#eraser").click(activateEraser);
+    $("#downloadImage").click(saveMyCanvas);
 
-	// activate the brush mode
-	activateBrush();
+    // activate the brush mode
+    activateBrush();
+}
+
+function drawOnCanvas(e) {
+    const brushWidth = document.querySelector("#brushWidthRange").value;
+    let color = null;
+
+    if (e.srcElement.parentElement && (e.srcElement.parentElement.id === "canvasArea" && modes.brush)) {
+        color = hexColor;
+        stroke(hexColor);
+        strokeWeight(brushWidth);
+        line(mouseX, mouseY, pmouseX, pmouseY);
+    } else if (e.srcElement.parentElement && (e.srcElement.parentElement.id === "canvasArea" && modes.eraser)) {
+        color = BACKGROUND_COLOR;
+        stroke(BACKGROUND_COLOR);
+        strokeWeight(brushWidth);
+        line(mouseX, mouseY, pmouseX, pmouseY);
+    }
+
+    let data = {
+        x: mouseX,
+        y: mouseY,
+        px: pmouseX,
+        py: pmouseY,
+        weight: brushWidth,
+        color,
+    };
+    socket.emit("mouse", data);
 }
 
 function mouseDragged(e) {
-	const brushWidth = document.querySelector("#brushWidthRange").value;
-	let color = null;
-	if (
-		e.srcElement.parentElement &&
-		(e.srcElement.parentElement.id === "canvasArea" && modes.brush)
-	) {
-		color = hexColor;
-		stroke(hexColor);
-		strokeWeight(brushWidth);
-		line(mouseX, mouseY, pmouseX, pmouseY);
-	} else if (
-		e.srcElement.parentElement &&
-		(e.srcElement.parentElement.id === "canvasArea" && modes.eraser)
-	) {
-		color = BACKGROUND_COLOR;
-		stroke(BACKGROUND_COLOR);
-		strokeWeight(brushWidth);
-		line(mouseX, mouseY, pmouseX, pmouseY);
-	}
-
-	let data = {
-		x: mouseX,
-		y: mouseY,
-		px: pmouseX,
-		py: pmouseY,
-		weight: brushWidth,
-		color
-	};
-	socket.emit("mouse", data);
+    drawOnCanvas(e);
 }
 
 function mouseClicked(e) {
-	const brushWidth = document.querySelector("#brushWidthRange").value;
-	let color = null;
-
-	if (
-		e.srcElement.parentElement &&
-		(e.srcElement.parentElement.id === "canvasArea" && modes.brush)
-	) {
-		color = hexColor;
-		stroke(hexColor);
-		strokeWeight(brushWidth);
-		line(mouseX, mouseY, pmouseX, pmouseY);
-	} else if (
-		e.srcElement.parentElement &&
-		(e.srcElement.parentElement.id === "canvasArea" && modes.eraser)
-	) {
-		color = BACKGROUND_COLOR;
-		stroke(BACKGROUND_COLOR);
-		strokeWeight(brushWidth);
-		line(mouseX, mouseY, pmouseX, pmouseY);
-	}
-
-	let data = {
-		x: mouseX,
-		y: mouseY,
-		px: pmouseX,
-		py: pmouseY,
-		weight: brushWidth,
-		color
-	};
-	socket.emit("mouse", data);
+    drawOnCanvas(e);
 }
 
 function clearScreen() {
-	background(BACKGROUND_COLOR);
-	socket.emit("clear");
+    background(BACKGROUND_COLOR);
+    socket.emit("clear");
 }
 
 function activateBrush() {
-	modes.brush = true;
-	modes.eraser = false;
+    modes.brush = true;
+    modes.eraser = false;
 
-	$("#brush").addClass("active");
-	$("#eraser").removeClass("active");
+    $("#brush").addClass("active");
+    $("#eraser").removeClass("active");
 }
 
 function activateEraser() {
-	modes.brush = false;
-	modes.eraser = true;
+    modes.brush = false;
+    modes.eraser = true;
 
-	$("#brush").removeClass("active");
-	$("#eraser").addClass("active");
+    $("#brush").removeClass("active");
+    $("#eraser").addClass("active");
 }
 
 function newDrawing(data) {
-	stroke(data.color);
-	strokeWeight(data.weight);
-	line(data.x, data.y, data.px, data.py);
+    stroke(data.color);
+    strokeWeight(data.weight);
+    line(data.x, data.y, data.px, data.py);
 }
 
 function saveMyCanvas() {
-	saveCanvas(canvas, "myCanvas", "png");
+    saveCanvas(canvas, "myCanvas", "png");
 }
